1bf3201fccf00dcff9e3c4a9230fc4af29fa7d65
MatK <mathew.kostrzewa@ontariotechu.net>
2020-10-04 13:07:38 -0400

    Added free camera and player toggle modes

---
+++ Assets/Scripts/CameraModeManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEngine;
+
+public class CameraModeManager : MonoBehaviour
+{
+    public GameObject playerObject;
+    public GameObject freeCameraObject;
+
+    public bool isFreeCameraMode;
+
+    void Start()
+    {
+        SetCameraMode(true);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            SetCameraMode(!isFreeCameraMode);
+        }
+    }
+
+    void SetCameraMode(bool a_isFreeCameraMode)
+    {
+        isFreeCameraMode = a_isFreeCameraMode;
+
+        if (a_isFreeCameraMode)
+        {
+            freeCameraObject.transform.position = playerObject.transform.position;
+            freeCameraObject.transform.rotation = playerObject.transform.rotation;
+
+            playerObject.SetActive(false);
+            freeCameraObject.SetActive(true);
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible   = true;
+        }
+        else
+
+        {
+            playerObject.transform.position = freeCameraObject.transform.position;
+
+            freeCameraObject.SetActive(false);
+            playerObject.SetActive(true);
+
+            playerObject.GetComponent<PlayerMotor>().rotationController.SetRotation(freeCameraObject.transform.eulerAngles);
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible   = false;
+        }
+    }
+}
